cb1dbd671268b87172782dd84629d2bb47c75187
Skulhunter5 <62426864+Skulhunter5@users.noreply.github.com>
2022-01-05 14:05:03 +0100

    Refactored MacroPreprocessor

--- src/steps/MacroPreprocessor.cs
+++ src/steps/MacroPreprocessor.cs
@@ -21,25 +21,31 @@ namespace IonS {
 
     class MacroPreprocessor {
 
-        private readonly Word[] _words;
+        private Word[] _words;
+        private Macro[] _macros;
 
         public MacroPreprocessor(Word[] words) {
             _words = words;
         }
 
-        private static Macro GetMacro(List<Macro> macros, string key) {
+        private Macro GetMacro(string key) {
+            foreach(Macro macro in _macros) if(macro.Key.Text == key) return macro;
+            return null;
+        }
+
+        private Macro GetMacro(List<Macro> macros, string key) {
             foreach(Macro macro in macros) if(macro.Key.Text == key) return macro;
             return null;
         }
 
-        private void expand(List<Macro> macros, Word[] _words, List<Word> words, int index) {
+        private void ExpandMacro(Word[] _words, List<Word> words, int index) {
             Word word = _words[index];
-            Macro macro = GetMacro(macros, word.Text);
+            Macro macro = GetMacro(word.Text);
             if(macro == null) return;
             for(int i = 0; i < macro.Words.Length; i++) {
                 Word word2 = macro.Words[i];
-                Macro macro2 = GetMacro(macros, word2.Text);
-                if(macro2 != null) expand(macros, macro.Words, words, i);
+                Macro macro2 = GetMacro(word2.Text);
+                if(macro2 != null) ExpandMacro(macro.Words, words, i);
                 else {
                     word2.ExpandedFrom = word;
                     words.Add(word2);
@@ -47,44 +53,55 @@ namespace IonS {
             }
         }
 
-        public MacroExpansionResult run() {
+        private Error CollectMacros() {
             List<Word> words = new List<Word>();
             List<Macro> macros = new List<Macro>();
             for(int i = 0; i < _words.Length; i++) {
                 Word word = _words[i];
                 if(word.Text == "macro") {
-                    if(i++ == _words.Length-1) return new MacroExpansionResult(null, new IncompleteMacroDefinitionError(word, null));
+                    if(i++ == _words.Length-1) return new IncompleteMacroDefinitionError(word, null);
                     Word key = _words[i];
 
-                    if(Keyword.isReserved(key.Text) || long.TryParse(key.Text, out long _)) return new MacroExpansionResult(null, new InvalidMacroKeyError(key));
+                    if(Keyword.isReserved(key.Text) || long.TryParse(key.Text, out long _)) return new InvalidMacroKeyError(key);
 
-                    if(i++ == _words.Length-2) return new MacroExpansionResult(null, new IncompleteMacroDefinitionError(word, key));
+                    if(i++ == _words.Length-2) return new IncompleteMacroDefinitionError(word, key);
 
                     Macro macro = GetMacro(macros, key.Text);
-                    if(macro != null) return new MacroExpansionResult(null, new MacroRedefinitionError(macro.Key, key));
+                    if(macro != null) return new MacroRedefinitionError(macro.Key, key);
 
                     List<Word> words1 = new List<Word>();
                     if(_words[i].Text != "{") words1.Add(_words[i]);
                     else {
                         while(true) {
                             i++;
-                            if(i >= _words.Length) return new MacroExpansionResult(null, new IncompleteMacroDefinitionError(word, key));
+                            if(i >= _words.Length) return new IncompleteMacroDefinitionError(word, key);
                             if(_words[i].Text == "}") break;
                             words1.Add(_words[i]);
                         }
                     }
                     macros.Add(new Macro(key, words1.ToArray()));
-                    continue;
-                } else {
-                    Macro macro = GetMacro(macros, word.Text);
-                    if(macro != null) {
-                        expand(macros, _words, words, i);
-                        continue;
-                    }
-                }
-                words.Add(word);
+                } else words.Add(word);
             }
-            return new MacroExpansionResult(words.ToArray(), null);
+            _words = words.ToArray();
+            _macros = macros.ToArray();
+            return null;
+        }
+
+        public Word[] ExpandMacros() {
+            List<Word> words = new List<Word>();
+            for(int i = 0; i < _words.Length; i++) {
+                Word word = _words[i];
+                Macro macro = GetMacro(word.Text);
+                if(macro != null) ExpandMacro(_words, words, i);
+                else words.Add(word);
+            }
+            return words.ToArray();
+        }
+
+        public MacroExpansionResult run() {
+            var result = CollectMacros();
+            if(result != null) return new MacroExpansionResult(null, result);
+            return new MacroExpansionResult(ExpandMacros(), null);
         }
     }
 
