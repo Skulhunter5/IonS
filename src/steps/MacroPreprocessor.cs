53f0f49855cb5013d3114d5c4467eeac685d4239
Skulhunter5 <62426864+Skulhunter5@users.noreply.github.com>
2022-01-05 13:47:20 +0100

    Implemented the usage of a macro inside of another macro

--- src/steps/MacroPreprocessor.cs
+++ src/steps/MacroPreprocessor.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 namespace IonS {
 
     class Macro {
-        public Macro(Word key, List<Word> words) {
+        public Macro(Word key, Word[] words) {
             Key = key;
             Words = words;
         }
 
         public Word Key { get; }
-        public List<Word> Words { get; }
+        public Word[] Words { get; }
     }
 
     class MacroExpansionResult : Result {
@@ -30,6 +30,21 @@ namespace IonS {
         private static Macro GetMacro(List<Macro> macros, string key) {
             foreach(Macro macro in macros) if(macro.Key.Text == key) return macro;
             return null;
+        }
+
+        private void expand(List<Macro> macros, Word[] _words, List<Word> words, int index) {
+            Word word = _words[index];
+            Macro macro = GetMacro(macros, word.Text);
+            if(macro == null) return;
+            for(int i = 0; i < macro.Words.Length; i++) {
+                Word word2 = macro.Words[i];
+                Macro macro2 = GetMacro(macros, word2.Text);
+                if(macro2 != null) expand(macros, macro.Words, words, i);
+                else {
+                    word2.ExpandedFrom = word;
+                    words.Add(word2);
+                }
+            }
         }
 
         public MacroExpansionResult run() {
@@ -58,15 +73,12 @@ namespace IonS {
                             words1.Add(_words[i]);
                         }
                     }
-                    macros.Add(new Macro(key, words1));
+                    macros.Add(new Macro(key, words1.ToArray()));
                     continue;
                 } else {
                     Macro macro = GetMacro(macros, word.Text);
                     if(macro != null) {
-                        foreach(Word word1 in macro.Words) {
-                            word1.ExpandedFrom = word;
-                            words.Add(word1);
-                        }
+                        expand(macros, _words, words, i);
                         continue;
                     }
                 }
