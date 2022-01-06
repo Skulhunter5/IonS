1867a63fec696067a32f98dae6897f0460c54be4
Skulhunter5 <62426864+Skulhunter5@users.noreply.github.com>
2022-01-06 23:32:52 +0100

    Small refactor of the preprocessors

--- src/steps/preprocessors/IncludePreprocessor.cs
+++ src/steps/preprocessors/IncludePreprocessor.cs
@@ -3,13 +3,6 @@ using System.Collections.Generic;
 
 namespace IonS {
 
-    class IncludePreprocessorResult : Result {
-        public IncludePreprocessorResult(Word[] words, Error error) : base(error) {
-            Words = words;
-        }
-        public Word[] Words { get; }
-    }
-
     class IncludePreprocessor {
 
         private readonly string _source;
@@ -35,27 +28,27 @@ namespace IonS {
             return temp;
         }
 
-        public IncludePreprocessorResult run() {
+        public PreprocessorResult run() {
             List<Word> words = new List<Word>();
             List<int> includes = new List<int>();
             for(int j = 0; j < _words.Length; j++) if(_words[j].Text == "include") includes.Add(j);
-            if(includes.Count == 0) return new IncludePreprocessorResult(_words, null);
-            if(includes[includes.Count-1] >= _words.Length - 1) return new IncludePreprocessorResult(null, new IncompleteIncludeError(_words[_words.Length-1].Position));
+            if(includes.Count == 0) return new PreprocessorResult(_words, null);
+            if(includes[includes.Count-1] >= _words.Length - 1) return new PreprocessorResult(null, new IncompleteIncludeError(_words[_words.Length-1].Position));
 
             int i = 0;
             while(i < includes.Count) {
                 for(int j = (i > 0 ? includes[i-1]+2 : 0); j < includes[i]; j++) words.Add(_words[j]);
                 Word nameWord = _words[includes[i]+1];
-                if(!nameWord.Text.StartsWith('"') || !nameWord.Text.EndsWith('"')) return new IncludePreprocessorResult(null, new FilePathNotAStringLiteralError(nameWord));
+                if(!nameWord.Text.StartsWith('"') || !nameWord.Text.EndsWith('"')) return new PreprocessorResult(null, new FilePathNotAStringLiteralError(nameWord));
                 string filename = AlterPath(_source, nameWord.Text.Substring(1, nameWord.Text.Length-2));
                 
-                if(!File.Exists(filename)) return new IncludePreprocessorResult(null, new FileNotFoundError(filename, Directory.GetCurrentDirectory(), nameWord.Position));
+                if(!File.Exists(filename)) return new PreprocessorResult(null, new FileNotFoundError(filename, Directory.GetCurrentDirectory(), nameWord.Position));
                 string path = Path.GetFullPath(filename);
                 string text = File.ReadAllText(path).Replace("\r\n", "\n");
                 var lexingResult = new Lexer(text, path).run();
-                if(lexingResult.Error != null) return new IncludePreprocessorResult(null, lexingResult.Error);
+                if(lexingResult.Error != null) return new PreprocessorResult(null, lexingResult.Error);
                 var incPreprocResult = new IncludePreprocessor(path, lexingResult.Words).run();
-                if(incPreprocResult.Error != null) return new IncludePreprocessorResult(null, incPreprocResult.Error);
+                if(incPreprocResult.Error != null) return new PreprocessorResult(null, incPreprocResult.Error);
                 foreach(Word word in incPreprocResult.Words) {
                     Word includedFrom = _words[includes[i]+1];
                     if(word.IncludedFrom == null) word.IncludedFrom = new Position[] {word.Position, includedFrom.Position}; // TODO: rework IncludedFrom (see ExpandedFrom)
@@ -65,7 +58,7 @@ namespace IonS {
             }
             for(int j = includes[i-1]+2; j < _words.Length; j++) words.Add(_words[j]);
 
-            return new IncludePreprocessorResult(words.ToArray(), null);
+            return new PreprocessorResult(words.ToArray(), null);
         }
 
     }
