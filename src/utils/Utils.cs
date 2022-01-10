f10a70de040bfc6b50849f49bcf37db6e69d42e7
Skulhunter5 <62426864+Skulhunter5@users.noreply.github.com>
2022-01-10 11:54:25 +0100

    Moved Keywords-class into separate file

--- src/utils/Utils.cs
+++ src/utils/Utils.cs
@@ -62,39 +62,4 @@ namespace IonS {
         public Position[] IncludedFrom { get; set; }
     }
     
-    class Keyword {
-        // Remember: Whenever new keyword is added: put it here
-        private static readonly string[] keywords = new string[] {
-            "exit",
-            "putc",
-            "drop", "2drop",
-            "dup", "2dup",
-            "over", "2over",
-            "swap", "2swap",
-            "rot", "2rot", "rot5", "2rot5",
-            "+", "-", "*", "/", "%", "/%",
-            "<<", ">>", "&", "|", "^", "inv",
-            "&&", "||", "not",
-            "min", "max",
-            "==", "!=", "<", ">", "<=", ">=",
-            ".",
-            "true", "false",
-            "if", "else", "while", "do",
-            "continue", "break",
-            "macro",
-            "var",
-            "include",
-            "here", "chere",
-            "{", "}",
-            "proc", "return", "inline",
-        };
-        public static bool isReserved(string word) {
-            if(word.StartsWith("@") || word.StartsWith("!")) return true;
-            if(word.StartsWith("syscall")) return true;
-            if(word.StartsWith("ctt")) return true;
-            foreach(string keyword in keywords) if(keyword == word) return true;
-            return false;
-        }
-    }
-    
 }
