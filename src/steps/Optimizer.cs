3c49086fcc5bccfa0186a2363ffaa0b24fae8960
Skulhunter5 <62426864+Skulhunter5@users.noreply.github.com>
2022-01-22 16:14:59 +0100

    Added optimizer

---
+++ src/steps/Optimizer.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+namespace IonS {
+
+    class Optimizer {
+
+        private readonly CodeBlock _root;
+
+        public Optimizer(CodeBlock root) {
+            _root = root;
+        }
+
+        private void OptimizeBlock(CodeBlock block) {
+            List<Operation> operations = new List<Operation>();
+            for(int i = 0; i < block.Operations.Count; i++) {
+                if(block.Operations[i].Type == OperationType.Drop) {
+                    int n = ((DropOperation) block.Operations[i]).N;
+                    while(i < block.Operations.Count-1 && block.Operations[i+1].Type == OperationType.Drop) n += ((DropOperation) block.Operations[++i]).N;
+                    operations.Add(new DropOperation(n, null));
+                } else operations.Add(block.Operations[i]);
+            }
+        }
+
+        public CodeBlock run() {
+            OptimizeBlock(_root);
+            return _root;
+        }
+
+    }
+
+}
